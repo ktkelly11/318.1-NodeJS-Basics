3ff7e71ab4cb1db4ff15e0623a067893440a03c2
Katherine Kelly <ktkelly11@gmail.com>
2024-05-18 21:02:06 -0400

    finished Part 5

--- part5.js
+++ part5.js
@@ -6,8 +6,21 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
-  res.write('<h1 style="color: red">Hello World!</h1>');
-  res.write("<p>I wonder what else we can send...</p>");
+  res.write(
+    `<h1 style="color: purple, text-alignment: center">Welcome to Katie's Page!</h1>`
+  );
+
+  switch (req.url) {
+    case "/plants":
+      response.write("Look at all of these cool plants");
+      break;
+    case "/places":
+      response.write("Look at all thses cool places!");
+      break;
+    case "/pics":
+      response.write("Look at all of these cool pics~");
+      break;
+  }
   res.end();
 });
 
